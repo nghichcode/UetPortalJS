a35c0e1644fe183c1df16e4038a6ae8ff75bd7f6
nghichcode <nghichcode@gmail.com>
2019-08-19 17:58:18 +0700

    renew 1908v2

---
+++ CodePortalv2.js
@@ -0,0 +1,7 @@
+// 1. Tạo
+$registrationAvailable=true
+// 2. Chuyển môn học sang môn toàn trường
+// 3. Tick các môn cần đăng ký
+// 4. Tạo nút "confirm-registration"
+$('#registration-container div[class="col-md-4 col-md-offset-0"]').append('<button confirm-registration"><i class="icon-save"></i> Ghi nhận</button>');
+// 5. Den gio thi click vao
